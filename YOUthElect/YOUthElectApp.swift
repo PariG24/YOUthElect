edb89e767f7a88378a3927d2ffe2350004766835
Pari Gulati <pgulati182@gmail.com>
2023-06-27 16:13:54 -0500

    Initial Commit

---
+++ YOUthElect/YOUthElectApp.swift
@@ -0,0 +1,17 @@
+//
+//  YOUthElectApp.swift
+//  YOUthElect
+//
+//  Created by Pari Gulati on 6/27/23.
+//
+
+import SwiftUI
+
+@main
+struct YOUthElectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ YOUthElectUITests/YOUthElectUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  YOUthElectUITestsLaunchTests.swift
+//  YOUthElectUITests
+//
+//  Created by Pari Gulati on 6/27/23.
+//
+
+import XCTest
+
+final class YOUthElectUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
