8a67522f1e281ea81f905dcd345c18799acdfa97
Pari Gulati <pgulati182@gmail.com>
2023-06-29 13:23:33 -0500

    Voting View New

--- YOUthElect/VoteOneView.swift
+++ YOUthElect/VoteOneView.swift
@@ -9,7 +9,11 @@ import SwiftUI
 
 struct VoteOneView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Text("Youth Empowerment Initiative: Shaping the Future of Oklahoma")
+            
+            
+        }
     }
 }
 
--- YOUthElect/VotingView.swift
+++ YOUthElect/VotingView.swift
@@ -20,68 +20,74 @@ struct VotingView: View {
                         Text("Youth Empowerment Initiative: Shaping the Future of Oklahoma")
                         
                     }
-                    .overlay(
-                        RoundedRectangle(cornerRadius: 15)
-                            .stroke(Color(red: 0.996, green: 0.498, blue: 0.471), lineWidth: 2)
-                    )
-                    .foregroundColor(.black)
                     .padding()
-                    /*.font(.body)
                     .foregroundColor(.black)
-                    .buttonStyle(.borderedProminent)
-                    //.border(Color(red: 0.996, green: 0.373, blue: 0.333), width: 5)
-                    .cornerRadius(15)
-                    .padding()
-                    .tint(Color(red: 0.996078431372549, green: 0.9764705882352941, blue: 1.0))*/
+                            .background(
+                                RoundedRectangle(
+
+                                    cornerRadius: 20,
+                                    style: .continuous
+                                )
+                                .stroke(Color(red: 0.996078431372549, green: 0.37254901960784315, blue: 0.3333333333333333), lineWidth: 2)
+
+                            )
+                 
                     
                     
                     
                     NavigationLink(destination: VoteTwoView()) {
                         Text("Name Of Second Movement")
-                    }
-                    
-                    .font(.body)
+                        
+                            }
+                    .padding()
                     .foregroundColor(.black)
-                    .buttonStyle(.borderedProminent)
-                    .border(Color(red: 0.996, green: 0.373, blue: 0.333))
-                    .tint(Color(red: 0.996078431372549, green: 0.9764705882352941, blue: 1.0))
-                    .cornerRadius(10)
+                            .background(
+                                RoundedRectangle(
+
+                                    cornerRadius: 20,
+                                    style: .continuous
+                                )
+                                .stroke(Color(red: 0.996078431372549, green: 0.37254901960784315, blue: 0.3333333333333333), lineWidth: 2)
+
+                            )
+                   
                     
                     NavigationLink(destination: VoteThreeView()) {
                         Text("Name of Third Movement")
                     }
-                    overlay(
-                        RoundedRectangle(cornerRadius: 15)
-                            .stroke(Color(red: 0.996, green: 0.498, blue: 0.471), lineWidth: 2)
-                    )
-                    .foregroundColor(.black)
+                    
                     .padding()
-                   /* .font(.body)
                     .foregroundColor(.black)
-                    .buttonStyle(.borderedProminent)
-                    .border(Color(red: 0.996, green: 0.373, blue: 0.333))
-                    .tint(Color(red: 0.996078431372549, green: 0.9764705882352941, blue: 1.0))
-                    .cornerRadius(10)*/
+                            .background(
+                                RoundedRectangle(
+
+                                    cornerRadius: 20,
+                                    style: .continuous
+                                )
+                                .stroke(Color(red: 0.996078431372549, green: 0.37254901960784315, blue: 0.3333333333333333), lineWidth: 2)
+
+                            )
+                  
                     
                     NavigationLink(destination: VoteFourView()) {
                         Text("Name of Fourth Movement")
                     }
-                    overlay(
-                        RoundedRectangle(cornerRadius: 15)
-                            .stroke(Color(red: 0.996, green: 0.498, blue: 0.471), lineWidth: 2)
-                    )
-                    .foregroundColor(.black)
                     .padding()
-                    /*.font(.body)
                     .foregroundColor(.black)
-                    .buttonStyle(.borderedProminent)
-                    .border(Color(red: 0.996, green: 0.373, blue: 0.333))
-                    .tint(Color(red: 0.996078431372549, green: 0.9764705882352941, blue: 1.0))
-                    .cornerRadius(10)*/
+                            .background(
+                                RoundedRectangle(
+
+                                    cornerRadius: 20,
+                                    style: .continuous
+                                )
+                                .stroke(Color(red: 0.996078431372549, green: 0.37254901960784315, blue: 0.3333333333333333), lineWidth: 2)
+
+                            )
                     
                     
                 }
                 .searchable(text: $searchText)
+               
         }
     }
 }
